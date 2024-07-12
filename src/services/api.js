6a599257fc8eec9093261397f512be9920b0d2b0
Vladyslav Rozhenko <pochta.vlad@ukr.net>
2024-07-12 13:20:14 +0300

    add form

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,18 @@
 import axios from "axios"
 
 axios.defaults.baseURL = "https://snake-server-kgz0.onrender.com"
+
 export const fetchUsers = async()=>{
 const {data} = await axios.get('/api/users')
 return data
+}
+
+export const addUser = async(name)=>{
+    const {data} = await axios.post('/api/users', name)
+    return data
+}
+
+export const addScore = async(userName, score)=>{
+    const {data} = await axios.post('/api/score', {user_name: userName, score})
+    return data
 }
